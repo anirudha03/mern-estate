032487d908dcff9101895df2cf29621fb383ab53
Anirudh Acharya <anirudhacharya134@gmail.com>
2023-12-09 14:01:35 +0530

    install react js

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-out" element={<SignOut/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
